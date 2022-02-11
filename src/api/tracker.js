5ddc8b706a988444546409c90f230a64b6a64f05
Meghan La Breche <Megavictoria@gmail.com>
2022-02-10 19:28:41 -0800

    reusable nav link

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-	baseURL: 'http://2195-2600-8801-df02-ca00-2430-2e03-25c-4b79.ngrok.io'
+	baseURL: 'http://290a-2600-8801-df02-ca00-d107-ffaa-c13-656f.ngrok.io'
 });
